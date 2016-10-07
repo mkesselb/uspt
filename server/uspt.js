54d26993c38a399ff3a5d0e73f7578f5b510e394
mkesselb <mkesselb@edu.aau.at>
2016-10-07 14:40:35 +0200

    add basic socket functionality for prototype

--- server/uspt.js
+++ server/uspt.js
@@ -4,8 +4,9 @@
 
 var mysql = require('mysql');
 var fs = require('fs');
-
+var crypto = require('crypto');
 var winston = require('winston')
+
 var express = require('express');
 var https = require('https');
 var http = require('http');
@@ -39,8 +40,39 @@ server.listen(port, function(err){
 //TODO: also support https -> options!
 //https.createServer(options, app).listen(443);
 
-io.on('connection', function(){
-	//TODO: connect to client websocket 
+//TODO: db connection
+//TODO: convert birthday to age
+//TODO: convert date.now() to a timestamp for db
+io.on('connection', function(socket){
+	logger.info('user connected');
 	
+	var user = {};
+	
+	socket.on('login', function(data){
+		//get login data from json
+		logger.info('login data received: ' + JSON.stringify(data));
+		
+		user.sex = data.sex;
+		user.age = data.birthday;
+		user.institution = data.institution;
+		user.skey = data.survey_key;
+		user.date = Date.now();
+		
+		logger.info('new user: ' + JSON.stringify(user));
+		
+		var hash = crypto.createHash('sha512');
+		hash.update(data.fname + data.lname + data.sex + data.birthday + data.institution + data.survey_key);
+		var hash_hex = hash.digest('hex');
+		
+		logger.info('hash value: ' + hash_hex);
+		
+		socket.emit('login-success', {hash: hash_hex});
+	});
+	
+	
+	
+	socket.on('disconnect', function(){
+		logger.info('user disconnected');
+	});
 });
 
